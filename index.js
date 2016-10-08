4994302856a19cd22ddd2fb637bae920031f5acf
Fantasy Shao <forever.fantasy27@gmail.com>
2016-10-08 14:18:02 +0800

    Add `excludeRules` option

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const glob = require('glob');
 
 const METHOD_ENUM = ['get', 'post', 'put', 'delete'];
 
-function loadRouter(app, root) {
+function loadRouter(app, root, options) {
   glob.sync(`${root}/**/*.js`).forEach((file) => {
     const filePath = file.replace(/\.[^.]*$/, '');
     const controller = require(filePath);
     const url = filePath.replace(root, '').replace(/\/index$/, '/');
     const methods = Object.keys(controller);
+
+    // Handle options
+    const excludeRules = options.excludeRules || [];
 
     methods.forEach((method) => {
       let handler = controller[method];
@@ -29,7 +32,9 @@ function loadRouter(app, root) {
           throw Error('[load-router]: invalid router definition: ', modifiedUrl);
       }
 
-      if (METHOD_ENUM.indexOf(methodLower) !== -1) {
+      if (excludeRules.indexOf(modifiedUrl) !== -1) {
+        // Nothing todo with the excluded rules
+      } else if (METHOD_ENUM.indexOf(methodLower) !== -1) {
         app[methodLower](modifiedUrl, handler);
       } else {
         throw Error('[load-router]: invalid method: ', methodLower);
