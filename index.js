547ed7532ed5f555fe19d2d100ce73acbde9dab0
Fantasy Shao <forever.fantasy27@gmail.com>
2016-10-08 14:46:02 +0800

    Add `rewriteRules` option

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ function loadRouter(app, root, options) {
   glob.sync(`${root}/**/*.js`).forEach((file) => {
     const filePath = file.replace(/\.[^.]*$/, '');
     const controller = require(filePath);
-    const url = filePath.replace(root, '').replace(/\/index$/, '/');
+    const url = filePath.replace(root, '').replace(/\/index$/, '');
     const methods = Object.keys(controller);
 
     // Handle options
     const excludeRules = options.excludeRules || [];
+    const rewriteRules = options.rewriteRules || new Map();
 
     methods.forEach((method) => {
       let handler = controller[method];
@@ -33,9 +34,11 @@ function loadRouter(app, root, options) {
       }
 
       if (excludeRules.indexOf(modifiedUrl) !== -1) {
-        // Nothing todo with the excluded rules
+        // Nothing to-do with the excluded rules
       } else if (METHOD_ENUM.indexOf(methodLower) !== -1) {
-        app[methodLower](modifiedUrl, handler);
+        rewriteRules.has(modifiedUrl) ?
+          app[methodLower](rewriteRules.get(modifiedUrl), handler) :
+          app[methodLower](modifiedUrl, handler);
       } else {
         throw Error('[load-router]: invalid method: ', methodLower);
       }
