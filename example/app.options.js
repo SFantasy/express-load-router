51411b87995c2f8995a5daf3d87b0d2e798be776
Fantasy Shao <forever.fantasy27@gmail.com>
2016-10-08 14:51:25 +0800

    Update examples

--- example/app.options.js
+++ example/app.options.js
@@ -5,6 +5,13 @@ const loadRouter = require('..');
 
 const app = express();
 
-loadRouter(app, path.join(__dirname, 'controllers'));
+loadRouter(app, path.join(__dirname, 'controllers'), {
+  excludeRules: [
+    '/product/list',
+  ],
+  rewriteRules: new Map([
+    ['/home', '/'],
+  ]),
+});
 
 app.listen(4000);
