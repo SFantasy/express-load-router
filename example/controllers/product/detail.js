3f170ef8acc1e1b6bb358bc5c034ef33a8dd43f8
Fantasy Shao <forever.fantasy27@gmail.com>
2016-10-13 19:17:23 +0800

    update: example

--- example/controllers/product/detail.js
+++ example/controllers/product/detail.js
@@ -1,5 +1,15 @@
 exports.GET = {
   params: [':id'],
+  middlewares: [
+    function (req, res, next) {
+      console.log('Middleware 1');
+      next();
+    },
+    function (req, res, next) {
+      console.log('Middleware 2');
+      next();
+    },
+  ],
   handler(req, res) {
     return res.send(`product detail ${req.params.id}`);
   },
